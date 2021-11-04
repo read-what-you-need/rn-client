5311464d12bb49a7651831fcdc80a5e155553857
soulbliss <deepak.garasangi@gmail.com>
2021-11-04 17:31:37 +0530

    add upload button

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.png";
 import "./App.scss";
 
-import LiveReadersTable from "./LiveReadersTable";
+import UploadButton from "./UploadButton/UploadButton";
 
 function App() {
   return (
@@ -10,14 +10,7 @@ function App() {
       <h1>Read what you need</h1>
       <div className="container">
         <div className="row">
-          <div className="col-sm-12 col-md-6 mx-auto mt-5">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="search for books"
-              aria-label="default input example"
-            />
-          </div>
+          <UploadButton />
         </div>
       </div>
     </div>
---
+++ src/components/UploadButton/UploadButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./UploadButton.scss";
+
+const UploadButton = () => {
+  return (
+    <div className="upload-button-wrapper">
+      <button className="upload-button">Upload book</button>
+    </div>
+  );
+};
+
+export default UploadButton;
