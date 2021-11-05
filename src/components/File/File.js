baa7a725970a32a1ea1f9e82e53865c09e2ee6ae
soulbliss <deepak.garasangi@gmail.com>
2021-11-05 16:20:00 +0530

    add file scss

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+import "./File.scss";
 const File = () => {
   let params = useParams();
   let id = params.id;
 
-  return <div className="App">I am file page {id}</div>;
+  return (
+    <div className="file-wrapper">
+      <span>I am file page {id}</span>
+      <input className="input-box-search-primary" type="search"></input>
+    </div>
+  );
 };
 
 export default File;
