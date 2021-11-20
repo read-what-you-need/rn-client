0425607cbd0c27e481ad7fe544d8fe05cb381c7b
soulbliss <deepak.garasangi@gmail.com>
2021-11-21 03:12:26 +0530

    remove list style

--- src/components/Line/LineItem.js
+++ src/components/Line/LineItem.js
@@ -2,13 +2,16 @@ import React from "react";
 
 const LineItem = ({ line }) => {
   return (
-    <ul>
+    <>
       <p>
         {" "}
-        {line.line} <br /> <code>score: {(line.score * 100).toString().slice(0, 5)}, line index: {line.line_index}</code>
+        {line.line} <br />{" "}
+        <code>
+          score: {(line.score * 100).toString().slice(0, 5)}, line index: {line.line_index}
+        </code>
       </p>
       <hr />
-    </ul>
+    </>
   );
 };
 
