da6ec128b46aa29c71869680d635f05caf99e21b
soulbliss <deepak.garasangi@gmail.com>
2021-11-20 19:09:12 +0530

    add top navBar

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,11 @@ import BookList from "./BooksList";
 function App() {
   return (
     <div className="App">
+      <div className="action-nav-bar">
+        <span className="nav-button">Login</span>
+        <span className="nav-button">/ Signup</span>
+        <span className="nav-button">/ User auth status : {false.toString()}</span>
+      </div>
       <h1 className="title">Read what you need</h1>
       <div className="container">
         <div className="row">
