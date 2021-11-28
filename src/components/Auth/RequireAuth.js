26fb1dd2bbb82053f006866115be085d9951b9dd
soulbliss <deepak.garasangi@gmail.com>
2021-11-28 14:08:10 +0530

    use local storage auth token

--- src/components/Auth/RequireAuth.js
+++ src/components/Auth/RequireAuth.js
@@ -1,21 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
-import userApi from "../../api/user";
 
 const RequireAuth = ({ children, redirectTo }) => {
-  const [isUserAuth, setIsUserAuth] = useState("loading");
-  useEffect(() => {
-    userApi.isUserAuth().then(res => {
-      console.log(res);
-      setIsUserAuth(res);
-    });
-  }, []);
-  console.log(`i am in private route: user auth status: ${isUserAuth}`);
-  if (isUserAuth === "loading") {
-    return <div>loading ...</div>;
-  } else {
-    return isUserAuth ? children : <Navigate to={redirectTo} />;
-  }
+  return localStorage.getItem('token') ? children : <Navigate to={redirectTo} />;
 };
 
 export default RequireAuth;
