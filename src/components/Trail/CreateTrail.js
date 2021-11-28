9124f14966cefd4ff679a08798f22f042b20f574
soulbliss <deepak.garasangi@gmail.com>
2021-11-28 14:37:09 +0530

    add trail state

--- src/components/Trail/CreateTrail.js
+++ src/components/Trail/CreateTrail.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from 'antd';
 import { Breadcrumb } from "antd";
 import { Input } from "antd";
+import "./CreateTrail.scss"
 
 const CreateTrail = () => {
   const [trailTitle, setTrailTitle] = useState();
+  const [trailLines, setTrailLines] = useState([]);
+
   console.log(trailTitle)
   return (
     <div className="App">
@@ -28,12 +32,11 @@ const CreateTrail = () => {
           <div className="col"></div>
           <div className="col">
             <h2>Trail title</h2>
-            <Input placeholder="Basic usage" onChange={(e) => setTrailTitle(e.target.value) }/>
-            <div className="row">Trail line 1</div>
-            <div className="row">Trail line 1</div>
-            <div className="row">Trail line 1</div>
+            <Input placeholder="Basic usage" size="large" onChange={(e) => setTrailTitle(e.target.value) }/>
+            <div className="row trail-header-title"><h3>{trailLines.length > 0 ? `Review` : `Add`} your trail</h3></div>
+            <div className="row trail-input-item"><Input placeholder="Basic usage" size="large" onChange={(e) => setTrailTitle(e.target.value) }/></div>
           </div>
-          <div className="col"></div>
+          <div className="col"><Button type="primary">Save trail</Button></div>
         </div>
       </div>
     </div>
