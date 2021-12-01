3bafb4e0133d76f8943cf57fa11af5d4bf9222da
soulbliss <deepak.garasangi@gmail.com>
2021-12-01 22:11:16 +0530

    add trail api

--- src/components/Trail/LinesTrail.js
+++ src/components/Trail/LinesTrail.js
@@ -4,19 +4,24 @@ import trailApi from "../../api/trail";
 import { Breadcrumb } from "antd";
 
 const LinesTrail = () => {
+  const [trailDetails, setTrailDetails] = useState([]);
   const [trailLines, setTrailLines] = useState([]);
   let params = useParams();
 
   useEffect(() => {
+    trailApi.getTrailDetails({ trailId: params.id }).then(res => setTrailDetails(res));
     trailApi.listUserTrailLines({ trailId: params.id, offset: 0, limit: 5 }).then(res => {
       setTrailLines(res);
     });
   }, []);
+
+  console.log(trailDetails)
   console.log(trailLines);
+
   return (
     <div className="App">
       <div className="container">
-        <h1>My trails</h1>
+        <h1>{trailDetails.title}</h1>
         <div className="breadcrumb">
           <Breadcrumb>
             <Breadcrumb.Item>
@@ -32,9 +37,7 @@ const LinesTrail = () => {
           </Breadcrumb>
         </div>
         <div className="row">
-          <div className="col">
-            
-          </div>
+          <div className="col"></div>
         </div>
       </div>
     </div>
