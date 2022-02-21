04bd3df070f3dca6ab3aa1e855a69c7f8f69866f
soulbliss <deepak.garasangi@gmail.com>
2022-02-21 22:20:32 +0530

    rm breadcrmbs and links

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,4 @@
 import "./App.scss";
-import { Link } from "react-router-dom";
-import { Breadcrumb } from "antd";
-import UploadButton from "./UploadButton/UploadButton";
 import BookList from "./BooksList";
 
 function App() {
@@ -9,17 +6,6 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="row">
-          <h1>My books</h1>
-          <div className="breadcrumb">
-            <Breadcrumb>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>
-              <Link to={`/user`}>Profile</Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>My books</Breadcrumb.Item>
-            </Breadcrumb>
-          </div>
-          <UploadButton />
           <BookList />
         </div>
       </div>
