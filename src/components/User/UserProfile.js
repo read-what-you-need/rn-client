28cc7028094e8420445d59fc2687237b95ee4a83
soulbliss <deepak.garasangi@gmail.com>
2022-02-21 20:49:33 +0530

    hero header main page

--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -1,23 +1,12 @@
 import React from "react";
 import "./UserProfile.scss";
-import { Breadcrumb } from "antd";
 import { Link } from "react-router-dom";
 
 const UserProfile = () => {
   return (
-    <div className="App">
-      <div className="container">
-        <h1>My profile</h1>
-        <div className="breadcrumb">
-          <Breadcrumb>
-            <Breadcrumb.Item>
-              <Link to={`/feed`}>Home</Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              <Link to={`/user`}>Profile</Link>
-            </Breadcrumb.Item>
-          </Breadcrumb>
-        </div>
+    <div className="user-profile">
+      <div className="user-profile-wrapper">
+        <h1 className="user-profile-hero-title">Search your book from the library</h1>
         <div className="row">
           <div className="col"></div>
           <div className="col-sm">
