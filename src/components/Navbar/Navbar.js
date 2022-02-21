41f6c0415f495c2c292be7c1ce69affa06d221a5
soulbliss <deepak.garasangi@gmail.com>
2022-02-21 20:04:41 +0530

    add navbar auth button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,49 +3,55 @@ import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import userApi from "../../api/user";
 
+import Logo from "../Icons/Logo.svg";
+import BookOpenIcon from "../Icons/BookOpenIcon.svg";
+import CompassIcon from "../Icons/CompassIcon.svg";
+import UserIcon from "../Icons/UserIcon.svg";
+import UploadIcon from "../Icons/UploadIcon.svg";
+
 const Navbar = () => {
-  let isUserAuth = localStorage.getItem('token') ? true : false;
-  return (
-    <div className="nav-bar-wrapper">
-       {isUserAuth? <NavbarAuth/> : <NavbarUnAuth />}
-      <span className="nav-button">/ Auth status : {isUserAuth.toString()}</span>
-    </div>
-  );
+  let isUserAuth = localStorage.getItem("token") ? true : false;
+  return <div className="nav-bar-wrapper">{isUserAuth ? <NavbarAuth /> : <NavbarUnAuth />}</div>;
 };
 
 const NavbarAuth = () => {
   return (
-    <>
-      <span className="nav-button">
-        <Link to={`/user`}>Home</Link>
+    <div className="nav-bar-auth-wrapper">
+      <span className="nav-logo">
+        <img src={Logo} alt="logo" />
       </span>
-      <span className="nav-button">
-        /{" "}
-        <Link
-          to={`/`}
-          onClick={() => {
-            userApi.logOutUser()
-          }}>
-          Logout
-        </Link>
-      </span>
-    </>
+      <div className="nav-central-action-icons">
+        <button className="nav-central-action-icon-button">
+          <img src={BookOpenIcon}></img>Your Books
+        </button>
+        <button className="nav-central-action-icon-button">
+          <img src={CompassIcon} alt="logo" /> Explore
+        </button>
+        <button className="nav-central-action-icon-button">Global Trails</button>
+      </div>
+      <div className="nav-upload-user-profile-button-wrapper">
+        <button className="nav-primary-action-button">
+          <img src={UploadIcon} alt="logo" /> Upload
+        </button>
+        <img src={UserIcon} alt="Your profile" />
+      </div>
+    </div>
   );
 };
 
 const NavbarUnAuth = () => {
   return (
     <>
-    <span className="nav-button">
-      <Link to={`/`}>Home</Link>
-    </span>
-    <span className="nav-button">
-      / <Link to={`/signin`}>Login</Link>
-    </span>
-    <span className="nav-button">
-      / <Link to={`/signup`}>Signup</Link>
-    </span>
-  </>
+      <span className="nav-button">
+        <Link to={`/`}>Home</Link>
+      </span>
+      <span className="nav-button">
+        / <Link to={`/signin`}>Login</Link>
+      </span>
+      <span className="nav-button">
+        / <Link to={`/signup`}>Signup</Link>
+      </span>
+    </>
   );
 };
 
