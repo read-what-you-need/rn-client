ccd19df98589fc479f53e56d79e0c63d453d0269
soulbliss <deepak.garasangi@gmail.com>
2022-02-26 16:57:21 +0530

    update redux store and add production check

--- src/reduxStore.js
+++ src/reduxStore.js
@@ -1,11 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
-import reducer from "./reducers";
+import reducers from "./reducers";
 
 const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = {};
 if (process.env.NODE_ENV !== "production") {
   middleware.push(createLogger());
+  store = createStore(reducers, composeEnhancers(applyMiddleware(...middleware)));
+} else {
+  store = createStore(reducers);
 }
 
-export default createStore(reducer, applyMiddleware(...middleware));
+export default store;
