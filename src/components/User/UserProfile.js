8f5c1cba5b7a745ff7d104046df18481978baa6e
soulbliss <deepak.garasangi@gmail.com>
2022-02-27 06:25:22 +0530

    refactor profile overview

--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -1,44 +1,14 @@
 import React from "react";
 import "./UserProfile.scss";
 import { Row, Col } from "antd";
-import { PencilEditIcon } from "../Icons";
+import UserProfileOverview from "./UserProfileOverview";
 import UserProfileActivityCards from "./UserProfileActivityCards";
 const UserProfile = () => {
   return (
     <div className="user-profile">
       <div className="user-profile-wrapper">
         <Row>
-          <div className="user-profile-overview-wrapper">
-            <div className="user-profile-overview-header">
-              <span className="user-profile-overview-header-text">Overview</span>
-            </div>
-            <div className="user-profile-overview-form">
-              <div className="user-profile-overview-form-fields">
-                <span className="user-profile-overview-form-field-label">name</span>
-                <span className="user-profile-overview-form-field-value">username</span>
-
-                <span className="user-profile-overview-form-field-label">email</span>
-                <span className="user-profile-overview-form-field-value">email</span>
-
-                <span className="user-profile-overview-form-field-label">joined</span>
-                <span className="user-profile-overview-form-field-value">date</span>
-              </div>
-              <div className="user-profile-overview-user-image-wrapper">
-                <div></div>
-                <div className="user-profile-overview-user-image-button">
-                  <button className="push-button not-selected">
-                    <PencilEditIcon />
-                    Edit profile
-                  </button>
-                </div>
-                <div className="user-profile-overview-user-image">
-                  <div className="circle">
-                    <span className="user-profile-image-circle-text">Upload</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <UserProfileOverview/>
         </Row>
         <Row>
           <Col span={8} className="tags-column">
---
+++ src/components/User/UserProfileOverview.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./UserProfileActivityCards.scss";
+import { PencilEditIcon } from "../Icons";
+import "./UserProfileOverview.scss";
+const UserProfileOverview = () => {
+  return (
+    <div className="user-profile-overview-wrapper">
+      <div className="user-profile-overview-header">
+        <span className="user-profile-overview-header-text">Overview</span>
+      </div>
+      <div className="user-profile-overview-form">
+        <div className="user-profile-overview-form-fields">
+          <span className="user-profile-overview-form-field-label">name</span>
+          <span className="user-profile-overview-form-field-value">username</span>
+
+          <span className="user-profile-overview-form-field-label">email</span>
+          <span className="user-profile-overview-form-field-value">email</span>
+
+          <span className="user-profile-overview-form-field-label">joined</span>
+          <span className="user-profile-overview-form-field-value">date</span>
+        </div>
+        <div className="user-profile-overview-user-image-wrapper">
+          <div></div>
+          <div className="user-profile-overview-user-image-button">
+            <button className="push-button not-selected">
+              <PencilEditIcon />
+              Edit profile
+            </button>
+          </div>
+          <div className="user-profile-overview-user-image">
+            <div className="circle">
+              <span className="user-profile-image-circle-text">Upload</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfileOverview;
