b87226ac957837f3fcc579b794d921642ebc56e4
soulbliss <deepak.garasangi@gmail.com>
2022-03-06 14:52:33 +0530

    add book info card

---
+++ src/components/Book/BookInfoCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./BookInfoCard.scss";
+const BookInfoCard = () => {
+  return <div className="book-info-card">I am book info card</div>;
+};
+
+export default BookInfoCard;
