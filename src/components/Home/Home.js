a985cdf2e4770b64ee44473ac7216b7f2b9c1424
soulbliss <deepak.garasangi@gmail.com>
2022-03-18 08:34:45 +0530

    add static svg

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import HomePageCarousel from "./HomePageCarousel";
+import { IntroduceLinesSVG, IntroduceTrailsSVG } from "../Icons";
 import "./Home.scss";
+import { Avatar } from "antd";
 function returnMainTitle() {
   return (
     <>
@@ -23,11 +25,13 @@ const Home = () => {
         <div className="home-page-info-section">
           <div></div>
           <div></div>
-          <div>anim 1</div>
+          <div>
+            <IntroduceLinesSVG />
+          </div>
           <div className="home-page-info-section-container">
             <div className="home-page-info-section-title">Rastero Answer Back</div>
             <div className="home-page-info-section-sub-title">
-              Ask any question to Rastero and he will give you answer from the book. Its like asking your friend in a natural way
+              Ask any question to Rastero and get your answer from the book. Its like asking your friend in a natural way
             </div>
             <div className="home-page-info-section-cta">
               <button className="push-button primary">Get Started</button>
@@ -46,9 +50,24 @@ const Home = () => {
             <div className="home-page-info-section-cta">
               <button className="push-button primary">Get Started</button>
             </div>
+            <div className="right-shift"></div>
+          </div>
+          <div className="right-shift">
+            <IntroduceTrailsSVG />
           </div>
         </div>
-        <div className="home-page-testimonial-section">Tweet fame</div>
+        <div className="home-page-testimonial-section">
+          <button className={"push-button secondary"}>
+            <div className="home-page-testimonial-section-reviewer-detail">
+              <Avatar src="https://pbs.twimg.com/profile_images/1500914711617015809/eFzyYWjS_400x400.jpg" /> <b>Jack Butcher</b>, Founder, Visualize
+              value
+            </div>
+            <div className="home-page-testimonial-section-text">“Really cool idea, can see it evolving in a lot of interesting ways. Nice work”</div>
+          </button>
+          <div className="home-page-testimonial-section-cta">
+            <button className="push-button primary">Get Started</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Home/HomePageCarousel.js
+++ src/components/Home/HomePageCarousel.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Carousel } from "antd";
 import "./HomePageCarousel.scss";
-const HomePageCarousel = () => {
+import { IntroduceListFormSVG, IntroduceProductSVG } from "../Icons";
 
+const HomePageCarousel = () => {
   return (
     <div className="home-page-carousel">
-      <Carousel  effect="fade">
+      <Carousel effect="fade">
         <div>
           <div className="home-page-carousel-card">
             <div className="home-page-carousel-card-info">
               <div className="home-page-carousel-card-title"></div>
               <div className="home-page-carousel-card-sub-title">Rastero is an AI-based model, who sorts out best important lines from your book</div>
             </div>
-            <div className="home-page-carousel-card-animation">Animation</div>
+            <div className="home-page-carousel-card-animation">
+              <IntroduceProductSVG />
+            </div>
           </div>
         </div>
 
@@ -24,22 +27,11 @@ const HomePageCarousel = () => {
                 Rastero, doesn't present you with bulky paragraphs, instead it shows small detailed lines in a list wise manner
               </div>
             </div>
-            <div className="home-page-carousel-card-animation">Animation</div>
-          </div>
-        </div>
-
-        <div>
-          <div className="home-page-carousel-card">
-            <div className="home-page-carousel-card-info">
-              <div className="home-page-carousel-card-title">Creates tag automatically</div>
-              <div className="home-page-carousel-card-sub-title">
-              All content is explorable based on tags created from your book/file which act as chapter or title 
-              </div>
+            <div className="home-page-carousel-card-animation">
+              <IntroduceListFormSVG />
             </div>
-            <div className="home-page-carousel-card-animation">Animation</div>
           </div>
         </div>
-
       </Carousel>
     </div>
   );
