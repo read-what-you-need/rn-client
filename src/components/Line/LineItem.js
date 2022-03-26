6185860fcf436822ee2fdb944640b843ad86d21b
soulbliss <deepak.garasangi@gmail.com>
2022-03-26 08:51:40 +0530

    rm confdience and question generator view

--- src/components/Line/LineItem.js
+++ src/components/Line/LineItem.js
@@ -1,48 +1,21 @@
 import React from "react";
 import "./LineItem.scss";
 import { connect } from "react-redux";
-import { onLineSelect, generateQuestion, onPressGeneratedQuestion } from "../../actions";
-import { message, Badge, Tag, Tooltip } from "antd";
-import { WaveIcon } from "../Icons";
-import { SyncOutlined } from "@ant-design/icons";
+import { onLineSelect,  } from "../../actions";
+import {  Badge,} from "antd";
 
-const questionGenerationFail = () => {
-  message.warning("Question could not be generated. Try again later.", 1);
-};
 
 const LineItem = ({
   line,
   onLineSelect,
   isSelected,
   feedback,
-  generateQuestion,
-  question,
-  isQuestionLoading,
-  questionForFileLineId,
-  onPressGeneratedQuestion
 }) => {
-  let score = line.score * 100;
-  score = score.toFixed(0);
-  let isLineItemActive = questionForFileLineId === line.file_line_id;
-  let isCurrenLineQuestionLoading = isQuestionLoading && isLineItemActive;
   return (
     <>
       <div className={`line-item ${isSelected ? "selected" : ""}`}>
         <div className="line-item-text" onClick={() => onLineSelect({ line })}>
           {line.line}
-        </div>
-        <div className="line-item-question" onClick={() => onPressGeneratedQuestion({ query: question })}>
-          {isLineItemActive && question}
-        </div>
-        <div className="line-action-items">
-          <Tooltip title={`Rastero is ${score}% confident`}>
-            <Tag color="default">{score}</Tag>
-          </Tooltip>
-          <div
-            className="line-action-item"
-            onClick={() => generateQuestion({ query: line.line, fileLineId: line.file_line_id }).catch(() => questionGenerationFail())}>
-            {!isCurrenLineQuestionLoading ? <WaveIcon /> : <SyncOutlined spin />}
-          </div>
         </div>
         <Badge color={feedback === 1 ? "green" : feedback === -1 ? "red" : ""}></Badge>
       </div>
@@ -56,5 +29,5 @@ const mapStateToProps = state => ({
   question: state.questionsWrapper?.question,
   questionForFileLineId: state.questionsWrapper?.questionForFileLineId
 });
-const actionCreators = { onLineSelect, generateQuestion, onPressGeneratedQuestion };
+const actionCreators = { onLineSelect };
 export default connect(mapStateToProps, actionCreators)(LineItem);
