4b7db912df24468b491d4dd175a6bb1ec54c6757
soulbliss <deepak.garasangi@gmail.com>
2022-04-01 02:49:48 +0530

    improve list

--- src/components/Book/BookSearch.js
+++ src/components/Book/BookSearch.js
@@ -1,18 +1,44 @@
-import React from "react";
-import { Input } from "antd";
+import React, { useState } from "react";
+import { Input, List, Avatar } from "antd";
+import { Link } from "react-router-dom";
 import fileApi from "../../api/file";
 
-function onEnterHandler(val) {
-  console.log(val);
-  if (val) {
-    fileApi.queryLibrary({ searchQuery: val });
-  }
-}
-
 const BookSearch = () => {
+  const [bookResults, setBookResults] = useState([]);
+  const [bookResultsLoading, setBookResultsLoading] = useState(false);
+
+  function onEnterHandler(val) {
+    setBookResultsLoading(true);
+    console.log(val);
+    if (val) {
+      fileApi.queryLibrary({ searchQuery: val }).then(data => {
+        setBookResults(data);
+        setBookResultsLoading(false);
+      });
+    }
+  }
   return (
     <div>
-      <Input onPressEnter={e => onEnterHandler(e.target.value)} placeholder="Basic usage" />
+      <Input onPressEnter={e => onEnterHandler(e.target.value)} placeholder="Basic search here" />
+
+      <List
+        itemLayout="horizontal"
+        dataSource={bookResults}
+        loading={bookResultsLoading}
+        renderItem={(item, index) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={<Avatar src={`https://picsum.photos/200/300?random=${index}`} />}
+              title={
+                <Link to={`/file/${item.md5}`}>
+                  <span className="book-page-breadcrumb-text">{item.title}</span>
+                </Link>
+              }
+              description={item.description}
+            />
+          </List.Item>
+        )}
+      />
     </div>
   );
 };
