1ccfdf215b1a5bb66fae04b1366a8134ba9f677f
soulbliss <deepak.garasangi@gmail.com>
2022-04-21 01:40:22 +0530

    update app env

--- src/reduxStore.js
+++ src/reduxStore.js
@@ -5,7 +5,7 @@ import reducers from "./reducers";
 const middleware = [thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = {};
-if (process.env.NODE_ENV !== "production") {
+if (process.env.REACT_APP_ENV !== "production") {
   store = createStore(reducers, composeEnhancers(applyMiddleware(...middleware)));
 } else {
   store = createStore(reducers);
