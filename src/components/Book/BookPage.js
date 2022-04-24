368fc160b750b0fb9c23d14312d114aa85ecdbbe
soulbliss <deepak.garasangi@gmail.com>
2022-04-24 21:21:50 +0530

    remove bread crumbs

--- src/components/Book/BookPage.js
+++ src/components/Book/BookPage.js
@@ -1,27 +1,10 @@
 import BookList from "../BooksList";
-import { Link } from "react-router-dom";
-import { RightArrowIcon } from "../Icons";
 
 import "./BookPage.scss";
 function Book() {
   return (
     <div className="book-page container">
       <div className="row">
-        <div className="book-page-breadcrumb">
-          <div>
-            <Link to={`/profile`}>
-              <span className="book-page-breadcrumb-text">Account</span>
-            </Link>
-          </div>
-          <div className="book-page-breadcrumb-icon">
-            <RightArrowIcon />
-          </div>
-          <div>
-            <Link to={`/books`}>
-              <span className="book-page-breadcrumb-text">Books</span>
-            </Link>
-          </div>
-        </div>
         <div className="book-page-header-wrapper">
           <div className="book-page-header-text">Your books</div>
         </div>
