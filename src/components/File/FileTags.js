4904aefa8dd852918e18671ccc0bdd193642d7e4
soulbliss <deepak.garasangi@gmail.com>
2022-04-25 01:12:42 +0530

    remove user auth initial state

--- src/components/File/FileTags.js
+++ src/components/File/FileTags.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Tooltip } from "antd";
 import { tagsCollapsible, onTagPress } from "../../actions";
 
 import { RightExpandIcon, LeftCollapseIcon } from "../Icons";
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -7,8 +7,6 @@ const initialState = {
 
 const user = (state = initialState, action) => {
   switch (action.type) {
-    case types.GET_USER_DETAILS_REQUEST:
-      return initialState;
     case types.GET_USER_DETAILS_REQUEST_SUCCESS:
       return { ...state, user: action.data };
     case types.GET_USER_DETAILS_REQUEST_FAILURE:
