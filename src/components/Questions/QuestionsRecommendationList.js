0530dd62442ade6a8e8db4db74b016b1f7d30c18
soulbliss <deepak.garasangi@gmail.com>
2022-05-21 16:44:08 +0530

    add skeleton loading

--- src/components/Questions/QuestionsRecommendationList.js
+++ src/components/Questions/QuestionsRecommendationList.js
@@ -2,20 +2,32 @@ import React from "react";
 import { connect } from "react-redux";
 import { onQuestionClick } from "../../actions";
 import "./QuestionsRecommendationList.scss";
+import { Skeleton } from "antd";
 
+const multiplySkeleton = byTimes => {
+  return (
+    <div>
+      {[...Array(byTimes).keys()].map(id => (
+        <Skeleton key={id} active />
+      ))}
+    </div>
+  );
+};
 const QuestionsRecommendationList = ({ onQuestionClick, isQuestionLoading, questionRecommendations }) => {
   return (
     <div className="question-recommendation-list">
-      {!isQuestionLoading ? questionRecommendations.map((question, index) => (
-        <span
-          className="question-recommendation-list-item"
-          key={index}
-          onClick={e => {
-            onQuestionClick({ question });
-          }}>
-          {question}
-        </span>
-      )): <>Loading question recommendation</>}
+      {!isQuestionLoading
+        ? questionRecommendations.map((question, index) => (
+            <span
+              className="question-recommendation-list-item"
+              key={index}
+              onClick={e => {
+                onQuestionClick({ question });
+              }}>
+              {question}
+            </span>
+          ))
+        : multiplySkeleton(3)}
     </div>
   );
 };
