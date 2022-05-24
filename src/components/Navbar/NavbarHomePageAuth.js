7370ed41278bd7f67db3ebf97da060438d1a894f
soulbliss <deepak.garasangi@gmail.com>
2022-05-25 00:40:22 +0530

    small screen navbar update

--- src/components/Navbar/NavbarHomePageAuth.js
+++ src/components/Navbar/NavbarHomePageAuth.js
@@ -4,10 +4,13 @@ import UploadButton from "../UploadButton/UploadButton";
 import { useNavigate, useLocation } from "react-router-dom";
 import UserMenu from "../User/UserMenu";
 import { BookOpenIcon, CompassIcon, Logo, GlobalIcon } from "../Icons";
+import useWindowDimensions from "../../libs/useWindowDimensions";
 
 const NavbarHomePageAuth = () => {
   let navigate = useNavigate();
   const location = useLocation();
+  const { height, width } = useWindowDimensions();
+  let isScreenSmall = width < 700;
   const currentPagePath = location.pathname;
   const navigateItems = [
     { icon: <BookOpenIcon />, displayText: "Your Books", path: "/recent" },
@@ -19,14 +22,14 @@ const NavbarHomePageAuth = () => {
       <span className="nav-logo" onClick={() => navigate("/recent")}>
         <Logo />
       </span>
-      <div className="nav-central-action-wrapper">
+      {!isScreenSmall && <div className="nav-central-action-wrapper">
         {navigateItems.map(item => (
           <button className={`nav-central-action-icon-button ${currentPagePath === item.path ? "active" : ""}`} onClick={() => navigate(item.path)}>
             {item?.icon}
             {item.displayText}
           </button>
         ))}
-      </div>
+      </div>}
       <div className="nav-corner-action-wrapper">
         <span className="nav-primary-action-button">
           <UploadButton />
